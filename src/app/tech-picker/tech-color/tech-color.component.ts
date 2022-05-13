3749b3f9613a236118f4ffc2392e99b98227dc42
haller <saltamontez78@gmail.com>
2022-05-13 16:31:57 +0200

    migration complete

--- src/app/tech-picker/tech-color/tech-color.component.ts
+++ src/app/tech-picker/tech-color/tech-color.component.ts
@@ -1,37 +1,36 @@
-import {Component, DoCheck, Input} from '@angular/core';
-import {TechColors} from '../../data';
-import {TechColor} from '../../tech-color.enum';
+import { Component, DoCheck, Input } from '@angular/core';
+import { TechColors } from '../../data';
+import { TechColor } from '../../tech-color.enum';
 
 @Component({
-    selector: 'app-tech-color',
-    templateUrl: './tech-color.component.html',
-    styleUrls: ['./tech-color.component.css']
+  selector: 'app-tech-color',
+  templateUrl: './tech-color.component.html',
+  styleUrls: ['./tech-color.component.css']
 })
 export class TechColorComponent implements DoCheck {
 
-    @Input() techColors: TechColors = [];
-    @Input() provided: TechColors = [];
+  @Input() techColors: TechColors = [];
+  @Input() provided: TechColors = [];
 
-    public colorEnum = TechColor;
-    public deltaTechColor: TechColors = [];
-    public Arr = Array;
+  public colorEnum = TechColor;
+  public deltaTechColor: TechColors = [];
+  public Arr = Array;
 
-    constructor() {
+  constructor() {
+  }
+
+  public updateDeltaTechColor() {
+    if (this.provided !== undefined) {
+      for (const color of Object.keys(this.techColors)) {
+        let index = Number.parseInt(color);
+        this.deltaTechColor[index] = this.techColors[index] - this.provided[index];
+      }
+    } else {
+      this.deltaTechColor = this.techColors;
     }
+  }
 
-    public updateDeltaTechColor() {
-        //TODO this is weird
-        if (this.provided !== undefined) {
-            for (const color of Object.keys(this.techColors)) {
-                let index = Number.parseInt(color);
-                this.deltaTechColor[index] = this.techColors[index] - this.provided[index];
-            }
-        } else {
-            this.deltaTechColor = this.techColors;
-        }
-    }
-
-    ngDoCheck() {
-        this.updateDeltaTechColor();
-    }
+  ngDoCheck() {
+    this.updateDeltaTechColor();
+  }
 }
--- src/app/tech-picker/tech/tech.component.ts
+++ src/app/tech-picker/tech/tech.component.ts
@@ -1,58 +1,50 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {RuntimeTech, TechColors} from '../../data';
-import {TechColor} from '../../tech-color.enum';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { RuntimeTech, TechColors } from '../../data';
+import { TechColor } from '../../tech-color.enum';
 
 @Component({
-    selector: 'app-tech',
-    templateUrl: './tech.component.html',
-    styleUrls: ['./tech.component.css'],
+  selector: 'app-tech',
+  templateUrl: './tech.component.html',
+  styleUrls: ['./tech.component.css'],
 })
 export class TechComponent implements OnInit {
 
-    @Input() tech: RuntimeTech = {
-        tech: {id: 0, name: "", requirements: [], description: "", provides: 0},
-        provided: {},
-        researched: false,
-        researchDistance: 0,
-        available: false
-    };
-    @Input() provided: TechColors = {};
-    @Output() researched: EventEmitter<RuntimeTech> = new EventEmitter<RuntimeTech>();
+  @Input() tech: RuntimeTech = {
+    tech: { id: 0, name: "", requirements: [], description: "", provides: 0 },
+    provided: {},
+    researched: false,
+    researchDistance: 0,
+    available: false
+  };
+  @Input() provided: TechColors = {};
+  @Output() researched: EventEmitter<RuntimeTech> = new EventEmitter<RuntimeTech>();
 
-    public showDescription = false;
+  public showDescription = false;
 
-    currentClasses: {} | undefined;
-    techColorClasses: {
-        'has-text-danger': boolean,
-        'has-text-success': boolean,
-        'has-text-warning': boolean,
-        'has-text-link': boolean,
-    } | undefined;
-
-    setCurrentClasses() {
-        this.currentClasses = {};
+  techColor(): string {
+    switch (this.tech.tech.provides) {
+      case TechColor.black:
+        return "white";
+      case TechColor.blue:
+        return "blue";
+      case TechColor.red:
+        return "red";
+      case TechColor.green:
+        return "green";
+      case TechColor.yellow:
+        return "yellow";
     }
+  }
 
-    setTechColorClasses() {
-        this.techColorClasses = {
-            'has-text-danger': this.tech.tech.provides === TechColor.red,
-            'has-text-success': this.tech.tech.provides === TechColor.green,
-            'has-text-warning': this.tech.tech.provides === TechColor.yellow,
-            'has-text-link': this.tech.tech.provides === TechColor.blue,
-        };
-    }
+  constructor() {
+  }
 
-    constructor() {
-    }
+  ngOnInit() {
+  }
 
-    ngOnInit() {
-        this.setTechColorClasses();
-        this.setCurrentClasses();
-    }
-
-    researchMe() {
-        this.researched.emit(this.tech);
-        this.setCurrentClasses();
-    }
+  researchMe(event: MouseEvent) {
+    event.cancelBubble = true;
+    this.researched.emit(this.tech);
+  }
 
 }
